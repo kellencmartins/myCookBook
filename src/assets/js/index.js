a252930323848c4fdaba3202123cf602b2a32554
kewng <kecandw@gmail.com>
2026-02-15 18:00:54 -0300

    front inicial

---
+++ src/assets/js/index.js
@@ -0,0 +1,18 @@
+let emptyState = document.querySelector('.state-empty');
+emptyState.classList.add('hidden');
+
+function addRecipe() {
+    window.location.href = 'add-recipe.html';
+}
+
+function btnReturn() {
+    window.location.href = 'index.html';
+}
+
+function btnCancel() {
+    window.location.href = 'index.html';
+}
+
+function recipe(){
+    window.location.href = 'recipe.html';
+}
